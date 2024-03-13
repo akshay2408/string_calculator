c6f3cc2ffbfa416a6bcea84f7aca6f03d0dcd59c
Akshay <akshaybabar2408@gmail.com>
2024-03-13 22:02:32 +0530

    add code and test for empty params

--- test_string_calculator.rb
+++ test_string_calculator.rb
@@ -4,5 +4,9 @@ require_relative 'string_calculator'
 class TestStringCalculator < Test::Unit::TestCase
   def setup
     @calculator = StringCalculator.new
-  end  
+  end
+
+  def test_empty_params
+    assert_equal(0, @calculator.add(nil))
+  end
 end  
